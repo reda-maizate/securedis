8c5bb8f138963996fc7726705567db81fed8ef91
Réda Maizate <reda.maizate@affluences.com>
2023-04-08 06:25:11 +0200

    feat: upgraded the organization of the different structs

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
 use std::io::{BufRead, BufReader};
 use std::net::{TcpListener, TcpStream};
 
-#[derive(Debug)]
+
+#[derive(Debug, Copy, Clone)]
 struct RESPHeader {
-    resp_type: RESPHeaderType,
-    num_of_elements: i32,
+    resp_type: Option<RESPHeaderType>,
+    num_of_elements: Option<i32>,
 }
 
 #[derive(Debug)]
 struct RESPObject {
-    header: Option<RESPHeader>,
-    elements: Vec<RESPElement>,
+    elements: Option<RESPElement>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct RESPElement {
-    // TODO: Ajouter un header pour bien tout binder
-    // header: RESPHeader,
-    content: String
+    header: RESPHeader,
+    content: Option<String>,
+    next: Box<Option<RESPElement>>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 enum RESPHeaderType {
     Array,
     BulkString,
@@ -38,17 +38,44 @@ enum Either<A, B> {
 impl RESPObject {
     fn new() -> RESPObject {
         RESPObject {
-            header: None,
-            elements: Vec::new(),
+            elements: None,
+        }
+    }
+}
+
+impl RESPElement {
+    fn new() -> RESPElement {
+        RESPElement {
+            header: RESPHeader {
+                resp_type: None,
+                num_of_elements: None,
+            },
+            content: None,
+            next: Box::new(None),
         }
     }
 }
 
 impl From<Vec<char>> for RESPElement {
-    fn from(s: Vec<char>) -> RESPElement {
-        let content: String = s.into_iter().collect();
+    fn from(str: Vec<char>) -> RESPElement {
+        let content: String = str.into_iter().collect();
         RESPElement {
-            content
+            header: RESPHeader { resp_type: None, num_of_elements: None },
+            content: Some(content),
+            next: Box::new(None),
+        }
+    }
+}
+
+impl From<RESPHeader> for RESPElement {
+    fn from(header: RESPHeader) -> RESPElement {
+        RESPElement {
+            header: RESPHeader {
+                resp_type: header.resp_type,
+                num_of_elements: header.num_of_elements
+            },
+            content: None,
+            next: Box::new(None),
         }
     }
 }
@@ -76,12 +103,12 @@ impl From<Vec<char>> for RESPHeader {
             // For Array, the next line is a header and the following lines are elements
             // TODO: Check for the others types
             RESPHeaderType::Array => RESPHeader {
-                resp_type,
-                num_of_elements: (num_of_elements * 2) as i32,
+                resp_type: Some(RESPHeaderType::Array),
+                num_of_elements: Some((num_of_elements * 2) as i32),
             },
             _ => RESPHeader {
-                resp_type,
-                num_of_elements,
+                resp_type: Some(resp_type),
+                num_of_elements: Some(num_of_elements),
             }
         }
     }
@@ -96,7 +123,7 @@ fn read_next_line(reader: &mut BufReader<TcpStream>, mut input: &mut String) -> 
     input.to_string()
 }
 
-fn read_header(input: &mut String) -> RESPHeader {
+fn read(input: &mut String) -> Vec<char> {
     let mut raw_chars: Vec<char> = input
         .chars()
         .collect();
@@ -104,59 +131,56 @@ fn read_header(input: &mut String) -> RESPHeader {
     // Remove the two last elements of the vector: \n and \r
     raw_chars.pop();
     raw_chars.pop();
-
-    let header: RESPHeader = raw_chars.into();
-    header
+    raw_chars
 }
 
-fn read_header_or_element(input: &mut String) -> Either<RESPHeader, RESPElement> {
-    let mut raw_chars: Vec<char> = input
-        .chars()
-        .collect();
+fn read_header(input: &mut String) -> RESPElement {
+    let cleaned_chars = read(input);
+    let header: RESPHeader = cleaned_chars.into();
+    header.into()
+}
 
-    // Remove the two last elements of the vector: \n and \r
-    raw_chars.pop();
-    raw_chars.pop();
+fn read_header_or_element(input: &mut String, resp_object: &mut RESPObject) -> () {
+    let cleaned_chars = read(input);
+    let last_element = resp_object.elements.as_mut().unwrap();
 
     // Check if this line is a header or an element
-    match raw_chars[0] {
+    match cleaned_chars[0] {
         '*' => {
-            let header: RESPHeader = raw_chars.into();
-            // println!("h: {:?}", header);
-            Either::Left(header)
+            let header: RESPHeader = cleaned_chars.into();
+            let mut element: RESPElement = header.into();
+            last_element.next = Box::new(Some(element.clone()));
         }
         '$' => {
-            let header: RESPHeader = raw_chars.into();
-            // println!("h: {:?}", header);
-            Either::Left(header)
+            let header: RESPHeader = cleaned_chars.into();
+            let mut element: RESPElement = header.into();
+            last_element.next = Box::new(Some(element.clone()));
         }
         ':' => {
-            let header: RESPHeader = raw_chars.into();
-            // println!("h: {:?}", header);
-            Either::Left(header)
+            let header: RESPHeader = cleaned_chars.into();
+            let mut element: RESPElement = header.into();
+            last_element.next = Box::new(Some(element.clone()));
         }
         '-' => {
-            let header: RESPHeader = raw_chars.into();
-            // println!("h: {:?}", header);
-            Either::Left(header)
+            let header: RESPHeader = cleaned_chars.into();
+            let mut element: RESPElement = header.into();
+            last_element.next = Box::new(Some(element.clone()));
         }
         '+' => {
-            let header: RESPHeader = raw_chars.into();
-            // println!("h: {:?}", header);
-            Either::Left(header)
+            let header: RESPHeader = cleaned_chars.into();
+            let mut element: RESPElement = header.into();
+            last_element.next = Box::new(Some(element.clone()));
         }
         // This is an element
         'A'..='Z' | 'a'..='z' | '0'..='9' => {
-            let element: RESPElement = raw_chars.into();
-            // println!("detected element : {:?}", element);
-            Either::Right(element)
+            last_element.content = Some(cleaned_chars.into_iter().collect());
         }
         _ => panic!("Unknown type of data"),
     }
 }
 
 
-fn process_request(request: RESPObject) -> () {
+fn process_request(_request: RESPObject) -> () {
     // TODO: Implement this but first we need to implement the RESP struct
 }
 
@@ -164,22 +188,21 @@ fn process_request(request: RESPObject) -> () {
 fn handle_connection(stream: TcpStream) -> () {
     let mut reader = BufReader::new(stream);
     let mut input = String::new();
-    let mut resp = RESPObject::new();
 
     let mut raw_header = read_next_line(&mut reader, &mut input);
 
     // Parse and print the first type and number of elements
-    let header = read_header(&mut raw_header);
-    println!("{:?}", header);
+    let mut resp_object = RESPObject::new();
+    let first_resp_element = read_header(&mut raw_header);
+    resp_object.elements = Some(first_resp_element.clone());
 
     // Read the elements and headers of next lines
-    for _ in 0..header.num_of_elements {
-        let mut nl = read_next_line(&mut reader, &mut input);
-        let nh = read_header_or_element(&mut nl);
-        println!("next header/element : {:?}", nh);
+    for _ in 0..first_resp_element.header.num_of_elements.unwrap() {
+        let mut new_parsed_line = read_next_line(&mut reader, &mut input);
+        read_header_or_element(&mut new_parsed_line, &mut resp_object);
     }
+    println!("{:#?}", resp_object);
 }
-
 
 fn main() {
     println!("Reda's redis server started...");
@@ -189,7 +212,6 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                println!("accepted new connection");
                 handle_connection(_stream)
             }
             Err(e) => {
