78eaf6300841323443a70f47d4cdfe8dde8d7385
Réda Maizate <reda.maizate@affluences.com>
2023-04-14 08:13:12 +0200

    chore: debug

--- spawn_redis_server.sh
+++ spawn_redis_server.sh
@@ -5,7 +5,7 @@
 # CodeCrafters uses this file to test your code. Don't make any changes here!
 #
 # DON'T EDIT THIS!
-export RUST_LOG="info"
+export RUST_LOG="debug"
 exec cargo run \
     --quiet \
     --release \
